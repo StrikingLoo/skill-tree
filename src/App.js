6bd4c2489ddc10638aa73614008fb26eb6d22169
strikingLoo <luciano.strika@mercadolibre.com>
2021-03-02 14:53:12 -0300

    hoping this works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import { SkillProvider, SkillTreeGroup, SkillTree } from 'beautiful-skill-tree';
+import { motion, harmony, cataclysm } from "./data/data";
 
 function App() {
   return (
     <div className="App">
       <div style={{ width: "500px", height: "150px", margin: "0 auto" }}>
+        <React.Fragment>
+          <SkillTree treeId="motion" title="Motion" data={motion} />
+          <SkillTree treeId="harmony" title="Harmony" data={harmony} />
+          <SkillTree treeId="cataclysm" title="Cataclysm" data={cataclysm} />
+        </React.Fragment>
         <img
           style={{ width: "500px", height: "200px" }}
           src={logo}
